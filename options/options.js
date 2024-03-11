c837bf8994090e796e3f1cd921bb5cadb3c29c22
Crowdbotics <team@crowdbotics.com>
2024-03-11 15:08:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shipping-quote-tool-46923",
+  url: "https://shipping-quote-tool-46923.botics.co",
+  api: "https://shipping-quote-tool-46923.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
